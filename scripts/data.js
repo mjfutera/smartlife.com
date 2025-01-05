e3578f1983ec8ed71effbe2599b766c5bb4de04d
Michal <mojasuperfirma@gmail.com>
2025-01-05 11:18:09 +0100

    05/01/2025

---
+++ scripts/data.js
@@ -0,0 +1,63 @@
+const data = {
+    "episodes": [
+        {
+            "title": "Test title",
+            "publicationTime": "01-01-2024",
+            "spotify": "spotifyLink",
+            "applePodcasts": "appleLink",
+            "youTube": "YoutTubelink",
+            "amazonPodcasts": "amazonLink",
+            "pandoraPodcasts": "pandoraLink",
+            "listenNotes": "listenNotesLink",
+            "castBoxPodcasts": "castboxLink",
+            "coverImage": "coverImageLink",
+            "keyWords": "keywords",
+            "noFollow": false
+        }
+    ],
+    "platforms": {
+        "spotify": {
+            "logo": "logolink",
+            "url": "http://onet.pl",
+            "title": "spotify Title",
+            "description": "description"
+        },
+        "applePodcasts": {
+            "logo": "logolink",
+            "url": "url",
+            "title": "title",
+            "description": "description"
+        },
+        "youTube": {
+            "logo": "logolink",
+            "url": "url",
+            "title": "title",
+            "description": "description"
+        },
+        "amazonPodcasts": {
+            "logo": "logolink",
+            "url": "url",
+            "title": "title",
+            "description": "description"
+        },
+        "pandoraPodcasts": {
+            "logo": "logolink",
+            "url": "url",
+            "title": "title",
+            "description": "description"
+        },
+        "listenNotes": {
+            "logo": "logolink",
+            "url": "url",
+            "title": "title",
+            "description": "description"
+        },
+        "castBoxPodcasts": {
+            "logo": "logolink",
+            "url": "url",
+            "title": "title",
+            "description": "description"
+        }
+    },
+    "logo": "logo"
+}
